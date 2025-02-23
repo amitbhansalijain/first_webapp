22abb035984cfb1eb284aa3924e36b0626986b07
amitbhansalijain <amitbhansali32@gmail.com>
2025-02-23 11:09:01 +0530

    new feature

--- myweb/myweb/myweb/myweb/settings.py
+++ myweb/myweb/myweb/myweb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
